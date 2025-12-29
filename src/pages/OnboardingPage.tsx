150dc1ecab25ed76afa4eccb659b447db9bab58d
dorhy <dorhyory@gmail.com>
2025-12-30 01:42:23 +0900

    refactor: svg 파일 불러오는 형식 수정

--- src/pages/OnboardingPage.tsx
+++ src/pages/OnboardingPage.tsx
@@ -1,35 +1,45 @@
 import { X } from "lucide-react";
-import Onboarding1_bg from "../assets/images/img_onboarding1_bg.svg?react";
-import Onboarding2 from "../assets/images/img_onboarding2.svg?react";
-import Onboarding2_frame_1 from "../assets/images/img_onboarding2_frame_1.svg?react";
-import Onboarding2_frame_2 from "../assets/images/img_onboarding2_frame_2.svg?react";
+import Onboarding1_bg from "@/assets/images/img_onboarding1_bg.svg";
+import Onboarding2 from "@/assets/images/img_onboarding2.svg";
+import Onboarding2_frame_1 from "@/assets/images/img_onboarding2_frame_1.svg";
+import Onboarding2_frame_2 from "@/assets/images/img_onboarding2_frame_2.svg";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination } from "swiper/modules";
-import Onboarding1_frame_1 from "../assets/images/img_onboarding1_frame_1.png";
-import Onboarding3_frame_1 from "../assets/images/img_onboarding3_frame_1.svg?react";
-import Onboarding3_frame from "../assets/images/img_onboarding3_frame.svg?react";
-import Onboarding3_text from "../assets/images/img_onboarding3_text.svg?react";
+import Onboarding1_frame_1 from "@/assets/images/img_onboarding1_frame_1.png";
+import Onboarding3_frame_1 from "@/assets/images/img_onboarding3_frame_1.svg";
+import Onboarding3_text from "@/assets/images/img_onboarding3_text.svg";
 import { useState } from "react";
+import { useNavigate } from "react-router";
+import "swiper/css";
+import "swiper/css/pagination";
 
 export const OnboardingPage = () => {
   const [isLast, setIsLast] = useState(false);
+  const navigate = useNavigate();
   return (
-    <main className="flex flex-col w-full h-dvh">
+    <main className="flex flex-col h-[100dvh] w-full overflow-hidden">
       {/* 닫기 버튼 */}
       <div className="flex justify-end w-full px-10 py-3">
-        <X className="cursor-pointer text-gray-600" />
+        <X
+          className="cursor-pointer text-gray-600"
+          onClick={() => navigate("/login")}
+        />
       </div>
       <Swiper
         className="w-full flex-1"
         modules={[Pagination]}
-        pagination={{ el: ".onboarding-pagination", type: "bullets" }}
+        pagination={{
+          el: ".onboarding-pagination",
+          type: "bullets",
+          clickable: true,
+        }}
         spaceBetween={0}
         slidesPerView={1}
         onSlideChange={(swiper) => {
           const isAtEnd = swiper.activeIndex === 2;
           setIsLast(isAtEnd);
           if (isAtEnd) {
-            swiper.allowSlidePrev = true;
+            swiper.allowSlidePrev = false;
           } else {
             swiper.allowSlidePrev = true;
           }
@@ -50,7 +60,10 @@ export const OnboardingPage = () => {
               alt=""
               className="absolute scale-70 z-10 top-1/2 left-1/2 -translate-x-1/2 -translate-y-[15%]"
             />
-            <Onboarding1_bg className="absolute top-42 right-0 w-full blur-[1px] [mask-image:linear-gradient(to_top,black_0%,transparent_100%)]" />
+            <img
+              src={Onboarding1_bg}
+              className="absolute top-42 right-0 w-full blur-[1px] [mask-image:linear-gradient(to_top,black_0%,transparent_100%)]"
+            />
           </div>
         </SwiperSlide>
         {/* Slide 2 */}
@@ -61,9 +74,18 @@ export const OnboardingPage = () => {
             <p className="ST2 text-gray-300">이미지로 이어지는 트라이브 챗</p>
           </div>
           <div className="relative flex-1 flex justify-center items-center">
-            <Onboarding2 className="absolute top-1/2 left-1/2 -translate-x-1/2" />
-            <Onboarding2_frame_1 className="absolute top-1/2 left-1/2 -translate-x-[85%] -translate-y-[-960%]" />
-            <Onboarding2_frame_2 className="absolute top-1/2 left-1/2 -translate-x-[95%] -translate-y-[-1200%]" />
+            <img
+              src={Onboarding2}
+              className="absolute top-1/2 left-1/2 -translate-x-1/2"
+            />
+            <img
+              src={Onboarding2_frame_1}
+              className="absolute top-1/2 left-1/2 -translate-x-[85%] -translate-y-[-960%] z-10"
+            />
+            <img
+              src={Onboarding2_frame_2}
+              className="absolute top-1/2 left-1/2 -translate-x-[95%] -translate-y-[-1200%] z-10"
+            />
           </div>
         </SwiperSlide>
         {/* Slide 3 */}
@@ -74,21 +96,24 @@ export const OnboardingPage = () => {
             <p className="ST2 text-gray-300">나만의 톤과 흐름이 됩니다.</p>
           </div>
           <div className="relative flex-1 flex justify-center">
-            <Onboarding3_frame_1 className="absolute top-0" />
+            <img src={Onboarding3_frame_1} className="absolute top-0" />
             <h1 className="absolute top-80 scale-[1.3] text-white H1">
               Vibers
             </h1>
-            <button className="absolute cursor-pointer top-94 bg-white px-10 py-3 rounded-[10px] scale-120 drop-shadow-[0_0_8px_rgba(250,250,250,0.4)] border border-gray-500">
-              <Onboarding3_text />
+            <button
+              onClick={() => navigate("/login")}
+              className="absolute cursor-pointer top-94 bg-white px-10 py-3 rounded-[10px] scale-120 drop-shadow-[0_0_8px_rgba(250,250,250,0.4)] border border-gray-500"
+            >
+              <img src={Onboarding3_text} />
             </button>
           </div>
         </SwiperSlide>
       </Swiper>
       <div
-        className={`onboarding-pagination flex justify-center gap-2 py-5 ${
+        className={`onboarding-pagination absolute bottom-0 w-full flex justify-center gap-2 pb-[env(safe-area-inset-bottom)] z-50 ${
           isLast ? "opacity-0 pointer-events-none" : "opacity-100"
         }`}
-      ></div>
+      />
     </main>
   );
 };
