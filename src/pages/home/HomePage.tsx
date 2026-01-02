bd03004b2681bffe93886cd7d5724f31c5d5003b
dorhy <dorhyory@gmail.com>
2026-01-02 19:53:41 +0900

    feat: home MY trace 부분 설계

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Img_3 from "@/assets/images/Img_3.png";
 import Icon_shortcut_quickdrop from "@/assets/icons/icon_shortcut_quickdrop.svg?react";
+import Icon_plus from "@/assets/icons/icon_plus.svg?react";
 
 const HomePage = () => {
   return (
-    <div className="flex flex-col min-h-[100dvh] w-full border-1 border-white">
+    <div className="flex flex-col min-h-[100dvh] w-full">
       {/* 헤더 */}
       <header className="relative flex flex-row justify-between items-end h-[55dvh] bg-cover bg-bottom bg-no-repeat">
         <div
@@ -12,28 +13,35 @@ const HomePage = () => {
           style={{
             backgroundImage: `url(${Img_3})`,
             maskImage:
-              "linear-gradient(to bottom, black 80%, transparent 100%)",
+              "linear-gradient(to bottom, black 70%, transparent 100%)",
             WebkitMaskImage:
-              "linear-gradient(to bottom, black 80%, transparent 100%)",
+              "linear-gradient(to bottom, black 70%, transparent 100%)",
           }}
         />
         <div className="flex flex-col">
-          <div className="B0 text-gray-100 px-4 z-10">
+          <div className="B0 text-gray-100 px-6 z-10">
             <p className="text-[24px]">Drop</p>
             <p className="text-[24px]">your vibe</p>
           </div>
-          <div className="H1 mb-4 px-4 z-10">
+          <div className="H1 mb-6 px-6 z-10">
             <h1 className="inline-block text-[28px] bg-[linear-gradient(to_right,white_70%,#8F9297_100%)] bg-clip-text text-transparent">
               #Minimal
             </h1>
           </div>
         </div>
-        <div className="z-10 mx-4 mb-7 cursor-pointer">
+        <div className="z-10 mx-4 mb-9 cursor-pointer">
           <Icon_shortcut_quickdrop />
         </div>
       </header>
       {/* My Trace */}
-      <section></section>
+      <section className="flex flex-col px-6 py-6 gap-4">
+        <h2 className="H2 text-gray-200">MY trace</h2>
+        <div className="flex">
+          <div className="w-[123px] h-[116px] bg-gray-900 rounded-[5px] flex justify-center items-center cursor-pointer border-dashed border-1 border-gray-800">
+            <Icon_plus className="h-[16px]" />
+          </div>
+        </div>
+      </section>
       {/* Categories */}
       <section></section>
     </div>
