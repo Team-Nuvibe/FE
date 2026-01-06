7af2914512ac0de3546e4b7733e59889067d164b
goody <ehdgusqaz98@gmail.com>
2026-01-06 13:30:59 +0900

    feat : mainlayout Navbar 상태관리 적용

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,8 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/common/Navbar";
 import QuickDropButton from "../components/common/QuickDropButton";
+import { useNavbarInfo } from "../hooks/useNavbarStore"; 
 
 const MainLayout = () => {
+  const isNavbarVisible = useNavbarInfo();
+
   return (
     <div className="relative w-full h-full min-h-screen bg-[var(--color-black)] text-white overflow-hidden">
       {/* 페이지 내용 */}
@@ -11,12 +14,14 @@ const MainLayout = () => {
       </main>
 
       {/* 하단 공통 컴포넌트 */}
-      <div className="fixed bottom-0 left-1/2 -translate-x-1/2 w-full max-w-[430px] h-0 z-50 overflow-visible pointer-events-none">
-        <div className="absolute bottom-8 left-1/2 -translate-x-1/2 flex items-center gap-2 pointer-events-auto">
-          <Navbar />
-          <QuickDropButton />
+      {isNavbarVisible && (
+        <div className="fixed bottom-0 left-1/2 -translate-x-1/2 w-full max-w-[430px] h-0 z-50 overflow-visible pointer-events-none">
+          <div className="absolute bottom-8 left-1/2 -translate-x-1/2 flex items-center gap-2 pointer-events-auto">
+            <Navbar />
+            <QuickDropButton />
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
