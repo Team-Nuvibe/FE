01a249ac3963511d3547734892dcf51e4d16ee05
goody <ehdgusqaz98@gmail.com>
2026-01-19 15:03:50 +0900

    feat : 로그인 API 구현

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -1,23 +1,39 @@
-// 회원가입
+import type { LogInRequest, LogInResponse, SignUpRequest } from "@/types/auth";
 import { axiosInstance } from "./axios";
-import type { ArchiveApiResponse } from "@/types/archive";
-
-// 회원가입 요청 타입
-export interface SignUpRequest {
-  name: string;
-  nickname: string;
-  email: string;
-  password: string;
-  confirmPassword: string;
-}
+import type { ApiResponse } from "@/types/common";
 
 // 회원가입
 export const signUp = async (
-  data: SignUpRequest,
-): Promise<ArchiveApiResponse<string>> => {
-  const response = await axiosInstance.post<ArchiveApiResponse<string>>(
+  body: SignUpRequest,
+): Promise<ApiResponse<string>> => {
+  const { data } = await axiosInstance.post<ApiResponse<string>>(
     "/api/auth/sign-up",
-    data,
+    body,
   );
-  return response.data;
+  return data;
+};
+
+// 로그인
+export const logIn = async (
+  body: LogInRequest,
+): Promise<ApiResponse<LogInResponse>> => {
+  const { data } = await axiosInstance.post<ApiResponse<LogInResponse>>(
+    "/api/auth/sign-in",
+    body,
+  );
+  return data;
+};
+
+// 로그아웃
+export const logOut = async (): Promise<ApiResponse<string>> => {
+  const { data } =
+    await axiosInstance.post<ApiResponse<string>>("/api/auth/logout");
+  return data;
+};
+
+// 회원탈퇴
+export const withDraw = async (): Promise<ApiResponse<string>> => {
+  const { data } =
+    await axiosInstance.delete<ApiResponse<string>>("/api/auth/withdraw");
+  return data;
 };
