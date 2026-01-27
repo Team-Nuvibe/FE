a8a4de85b60e7fabfe41d31adc4164599eb46d9e
dorhy <dorhyory@gmail.com>
2026-01-27 20:03:31 +0900

    feat: 스크랩 페이지 UI 구현 및 라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -25,6 +25,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import TribechatPage from "./pages/tribe-chat/TribechatPage";
 import TribechatRoomPage from "./pages/tribe-chat/TribechatRoomPage";
 import { SplashLayout } from "./layouts/SplashLayout";
+import { ScrapPage } from "./pages/tribe-chat/ScrapPage";
 
 // 인증 없이 접근 가능한 라우트
 const publicRoutes: RouteObject[] = [
@@ -69,6 +70,7 @@ const protectedRoutes: RouteObject[] = [
           { index: true, element: <TribechatPage /> },
           { path: "/tribe-chat:tagid", element: <TribechatRoomPage /> },
           // {path: "/tribe-chat:tagid/album", element: </> },
+          { path: "/tribe-chat/scrap", element: <ScrapPage /> },
         ],
       },
       {
