c79fa6b8dff78edc7a357bcfb15b9ec7e7011f25
dorhyory <dorhyory@gmail.com>
2026-01-31 00:16:04 +0900

    feat: 온보딩 페이지 변경점 반영

--- src/pages/onboarding/OnboardingPage.tsx
+++ src/pages/onboarding/OnboardingPage.tsx
@@ -9,6 +9,11 @@ import Onboarding1_frame_1 from "@/assets/images/img_onboarding1_frame_1.png";
 import Onboarding3_frame from "@/assets/images/img_onboarding3_frame.png";
 import Onboarding3_text from "@/assets/images/img_onboarding3_text.svg";
 import IconXbuttonGray3 from "@/assets/icons/icon_xbutton_gray3.svg?react";
+import Onboarding1_1 from "@/assets/images/img_onboarding1_1.svg";
+import Onboarding1_2 from "@/assets/images/img_onboarding1_2.svg";
+import Onboarding2_1 from "@/assets/images/img_onboarding2_1.svg";
+import Onboarding3_1 from "@/assets/images/img_onboarding3_1.svg";
+import Onboarding3_2 from "@/assets/images/img_onboarding3_2.svg";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import "swiper/css";
@@ -44,72 +49,59 @@ export const OnboardingPage = () => {
         <SwiperSlide className="flex w-full flex-col">
           <div className="mt-[40px] mb-[45px] flex shrink-0 flex-col items-center gap-[3px]">
             <h1 className="H0 tracking-tight text-white">Drop your vibe.</h1>
-            <div className="flex flex-col items-center">
-              <p className="ST2 tracking-tight text-gray-300">
+              <p className="B1 tracking-tight text-gray-300 text-center">
                 이미지를 드롭하고,
-              </p>
-              <p className="ST2 tracking-tight text-gray-300">
+              <br/>
                 태그로 지금의 감각을 기록하세요.
               </p>
-            </div>
           </div>
           <div className="relative flex flex-1 items-center justify-center">
+            <img src={Onboarding1_1} className="absolute -top-7.5 z-10" />
             <img
-              src={Onboarding1_frame_1}
-              alt=""
-              className="absolute top-1/2 left-1/2 z-10 -translate-x-1/2 -translate-y-[11%] scale-80"
-            />
-            <img
-              src={Onboarding1_bg}
-              className="absolute top-45 right-0 w-full [mask-image:linear-gradient(to_top,black_0%,transparent_100%)] blur-[1px]"
+              src={Onboarding1_2}
+              className="absolute top-45 right-0 w-full [mask-image:linear-gradient(to_top,black_0%,transparent_100%)] blur-[1.3px]"
             />
           </div>
         </SwiperSlide>
         {/* Slide 2 */}
         <SwiperSlide className="flex w-full flex-col">
-          <div className="mt-[40px] shrink-0 px-12">
-            <h1 className="H0 mb-2 text-white">Find your tribe.</h1>
-            <p className="ST2 text-gray-300">말 없이도 통하는 감각.</p>
-            <p className="ST2 text-gray-300">이미지로 이어지는 트라이브 챗</p>
+          <div className="mt-[40px] mb-[45px] flex shrink-0 flex-col items-center gap-[3px]">
+            <h1 className="H0 tracking-tight text-white">Find your tribe.</h1>
+              <p className="B1 tracking-tight text-gray-300 text-center">
+                말 없이도 통하는 감각.
+              <br/>
+                이미지로 이어지는 트라이브 챗
+              </p>
           </div>
           <div className="relative flex flex-1 items-center justify-center">
             <img
-              src={Onboarding2}
-              className="absolute top-1/2 left-1/2 -translate-x-1/2"
-            />
-            <img
-              src={Onboarding2_frame_1}
-              className="absolute top-1/2 left-1/2 z-10 -translate-x-[85%] -translate-y-[-960%]"
-            />
-            <img
-              src={Onboarding2_frame_2}
-              className="absolute top-1/2 left-1/2 z-10 -translate-x-[95%] -translate-y-[-1200%]"
+              src={Onboarding2_1}
+              className="absolute -top-1.5 translate-x-4"
             />
           </div>
         </SwiperSlide>
         {/* Slide 3 */}
         <SwiperSlide className="flex w-full flex-col">
-          <div className="mt-[40px] shrink-0 px-12">
-            <h1 className="H0 mb-2 text-white">Your vibe, archived.</h1>
-            <p className="ST2 text-gray-300">드롭한 이미지들이 보드에 쌓여</p>
-            <p className="ST2 text-gray-300">나만의 톤과 흐름이 됩니다.</p>
+          <div className="mt-[40px] mb-[45px] flex shrink-0 flex-col items-center gap-[3px]">
+            <h1 className="H0 tracking-tight text-white">Your vibe, archived.</h1>
+              <p className="B1 tracking-tight text-gray-300 text-center">
+                드롭한 이미지들이 보드에 쌓여
+              <br/>
+                나만의 톤과 흐름이 됩니다.
+              </p>
           </div>
           <div className="relative flex flex-1 flex-col items-center justify-center gap-10">
             <img
-              src={Onboarding3_frame}
-              className="max-h-[50vh] w-[75%] -translate-y-[-10%] object-contain"
+              src={Onboarding3_1}
+              className="absolute top-0 object-contain"
             />
-            <div className="z-10 flex flex-col items-center gap-6">
-              <h1 className="H1 -translate-y-2 scale-[1.2] text-white">
-                Vibers
-              </h1>
-              <button
-                onClick={() => navigate("/login")}
-                className="scale-100 cursor-pointer rounded-[10px] border border-gray-500 bg-white px-10 py-3 drop-shadow-[0_0_8px_rgba(250,250,250,0.4)]"
-              >
-                <img src={Onboarding3_text} />
-              </button>
-            </div>
+            <img src={Onboarding3_2} className="absolute top-[250px]"/>
+            <button
+              onClick={() => navigate("/login")}
+              className="absolute top-[416px] cursor-pointer rounded-[10px] border-[1px] border-gray-100/50 bg-white px-10 py-[5px] drop-shadow-[0_0_12px_rgba(250,250,250,0.6)]"
+            >
+              <p className="font-medium text-[20px] bg-[linear-gradient(to_right,black,rgba(0,0,0,0.5))] bg-clip-text text-transparent">Start Nuvibe</p>
+            </button>
           </div>
         </SwiperSlide>
       </Swiper>
