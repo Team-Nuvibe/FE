129f1174778814c9aeb4ee0d31b2be3d22f6b17e
goody <ehdgusqaz98@gmail.com>
2026-02-02 02:36:49 +0900

    feat : 가이드 애니메이션 비디오 파일로 적용

--- src/components/archive-board/vibecalendar/MovingDotAnimation.tsx
+++ src/components/archive-board/vibecalendar/MovingDotAnimation.tsx
@@ -1,10 +1,10 @@
-import drawingGuideMotion from "@/assets/animation/drawing_guide_motion.gif";
+import drawingGuideMotion from "@/assets/animation/drawing_guide_motion.webm";
 
 const MovingDotAnimation = () => {
   return (
-    <div className="pointer-events-none flex h-full w-full flex-col items-center justify-center">
-      {/* 안내 텍스트 */}
-      <div className="z-10 shrink-0 space-y-1 text-center select-none">
+    <div className="pointer-events-none relative h-full w-full">
+      {/* 안내 텍스트 - 상단에 절대 위치 */}
+      <div className="absolute top-12 left-1/2 z-10 -translate-x-1/2 space-y-1 text-center select-none">
         <p className="H2 leading-[150%] text-black">
           손끝을 따라
           <br />
@@ -12,12 +12,15 @@ const MovingDotAnimation = () => {
         </p>
       </div>
 
-      {/* GIF 애니메이션 */}
-      <div className="relative z-10 flex w-full flex-1 items-center justify-center">
-        <img
+      {/* WEBM 비디오 애니메이션 - 중앙에 절대 위치 */}
+      <div className="absolute top-1/2 left-1/2 z-10 w-full -translate-x-1/2 -translate-y-1/2">
+        <video
           src={drawingGuideMotion}
-          alt="Drawing guide animation"
-          className="relative z-10 h-full w-full object-contain mix-blend-screen invert"
+          autoPlay
+          loop
+          muted
+          playsInline
+          className="h-[481px] w-[361px] object-contain mix-blend-screen invert"
         />
       </div>
     </div>
